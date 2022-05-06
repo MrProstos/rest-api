f0fd9db45262a507a40d4a0d2796322e08cf5d37
MrProstos <vladmihin28@gmail.com>
2022-05-06 18:10:51 +0300

    ю

--- main.go
+++ main.go
@@ -4,20 +4,8 @@ import (
 	"fmt"
 
 	"github.com/MrProstos/rest-api/db"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", db.Db_host, db.Db_user, db.Db_name, db.Db_pass)
-	fmt.Println(dbUri)
-
-	conn, err := gorm.Open(db.Db_type, dbUri)
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Println(conn.Error)
-	user := db.Operator{Username: "Vlad", Token: "test"}
-	user.Del(conn)
-
+	fmt.Println(db.DB)
 }
