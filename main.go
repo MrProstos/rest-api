4eb68e5b83c885e739fffa635eea0977c6280cdc
MrProstos <vladmihin28@gmail.com>
2022-05-21 00:41:08 +0300

    Добавил маршруты

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/addclient/", server.AddClient).Methods("POST")
 	router.HandleFunc("/updateclient/", server.UpdateClient).Methods("POST")
+	router.HandleFunc("/delclient/", server.DelClient).Methods("POST")
+	router.HandleFunc("/addorder/", server.AddOrder).Methods("POST")
+	router.HandleFunc("/updateorder/", server.UpdateOrder).Methods("POST")
+	router.HandleFunc("/delorder/", server.DelOrder).Methods("POST")
 	err := http.ListenAndServe(":2000", router)
 	if err != nil {
 		log.Fatal(err)
