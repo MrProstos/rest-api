ae1d590e1a2e79f665bf26b1e88c45852433b10d
MrProstos <vladmihin28@gmail.com>
2022-05-21 00:42:40 +0300

    Добавил тесты

--- server/server_test.go
+++ server/server_test.go
@@ -7,61 +7,123 @@ import (
 	"io/ioutil"
 	"net/http"
 	"testing"
+
+	"github.com/MrProstos/rest-api/db"
 )
 
 func TestAddClient(t *testing.T) {
-	userData := map[string]interface{}{
-		"phone_num": "888",
-		"firstname": "vlad",
-		"lastname":  "vlad",
-		"birthday":  "1-1-2000",
-	}
 
-	strJSON, err := json.Marshal(userData)
-	if err != nil {
-		t.Error(err)
-	}
+	array := []db.Client{{
+		Phone_num: "777",
+		Firstname: "Vlad",
+		Lastname:  "Mikhin",
+		Birthday:  "1999-07-22",
+	}, {
+		Phone_num: "666",
+		Firstname: "Test",
+		Lastname:  "Test",
+		Birthday:  "00-00-0000",
+	}, {
+		Phone_num: "123456789",
+		Firstname: "QQQQ",
+		Lastname:  "ZZZZ",
+		Birthday:  "0000000",
+	}}
 
-	body, err := http.Post("http://localhost:2000/addclient/", "application/json", bytes.NewBuffer(strJSON))
-	if err != nil {
-		t.Error(err)
-	}
-	b, _ := ioutil.ReadAll(body.Body)
+	for i := range array {
 
-	if string(b) != "200" {
-		t.Error(string(b))
+		strJSON, err := json.Marshal(array[i])
+		if err != nil {
+			t.Error(err)
+		}
+
+		body, err := http.Post("http://localhost:2000/addclient/", "application/json", bytes.NewBuffer(strJSON))
+		if err != nil {
+			t.Error(err)
+		}
+		b, _ := ioutil.ReadAll(body.Body)
+
+		if string(b) != "200" {
+			t.Error(string(b))
+		}
+		fmt.Printf("Тест номер %v %v\n", i+1, string(b))
 	}
-	fmt.Println(string(b))
 }
 
 func TestUpdateClient(t *testing.T) {
-	userData := map[string]interface{}{
-		"id":        22,
-		"phone_num": "xxx",
-		"firstname": "xxx",
-		"lastname":  "xxx",
-		"birthday":  "xxx",
-	}
+	array := []db.Client{{
+		ID:        1,
+		Phone_num: "UPDAT1",
+		Firstname: "UPDATE",
+		Lastname:  "UPDATE",
+		Birthday:  "UPDATE",
+	}, {
+		ID:        2,
+		Phone_num: "UPDATE2",
+		Firstname: "UPDATE",
+		Lastname:  "UPDATE",
+		Birthday:  "UPDATE",
+	}, {
+		ID:        3,
+		Phone_num: "UPDATE3",
+		Firstname: "UPDATE",
+		Lastname:  "UPDATE",
+		Birthday:  "UPDATE",
+	}}
 
-	strJSON, err := json.Marshal(userData)
-	if err != nil {
-		t.Error(err)
-	}
+	for i := range array {
 
-	body, err := http.Post("http://localhost:2000/updateclient/", "application/json", bytes.NewBuffer(strJSON))
-	if err != nil {
-		t.Error(err)
-	}
+		strJSON, err := json.Marshal(array[i])
+		if err != nil {
+			t.Error(err)
+		}
 
-	b, _ := ioutil.ReadAll(body.Body)
+		body, err := http.Post("http://localhost:2000/updateclient/", "application/json", bytes.NewBuffer(strJSON))
+		if err != nil {
+			t.Error(err)
+		}
 
-	if string(b) != "200" {
-		t.Error(string(b))
+		b, _ := ioutil.ReadAll(body.Body)
+
+		if string(b) != "200" {
+			t.Error(string(b))
+		}
+		fmt.Printf("Тест номер %v %v\n", i+1, string(b))
 	}
-	fmt.Println(string(b))
 }
 
-func TestEnterOrder(t *testing.T) {
+func TestDelClietn(t *testing.T) {
+	array := []db.Client{{
+		ID: 1,
+	}, {
+		ID: 2,
+	}, {
+		ID: 3,
+	}}
+
+	for i := range array {
+
+		strJSON, err := json.Marshal(array[i])
+		if err != nil {
+			t.Error(err)
+		}
+
+		body, err := http.Post("http://localhost:2000/delclient/", "application/json", bytes.NewBuffer(strJSON))
+		if err != nil {
+			t.Error(err)
+		}
+
+		b, _ := ioutil.ReadAll(body.Body)
+
+		if string(b) != "200" {
+			t.Error(string(b))
+		}
+		fmt.Printf("Тест номер %v %v\n", i+1, string(b))
+	}
+
+}
+
+func TestAddOrder(t *testing.T) {
 	orderData := map[string]interface{}{
 		"Client_id": 3,
 	}
@@ -76,7 +138,30 @@ func TestEnterOrder(t *testing.T) {
 		t.Error(err)
 	}
 	b, _ := ioutil.ReadAll(body.Body)
-	if string(b) != "Succses!" {
+	if string(b) != "200" {
+		t.Error(string(b))
+	} else {
+		fmt.Println(string(b))
+	}
+}
+
+func TestUpdateOrder(t *testing.T) {
+
+	orderData := map[string]interface{}{
+		"Client_id": 3,
+	}
+
+	strJSON, err := json.Marshal(orderData)
+	if err != nil {
+		t.Error(err)
+	}
+
+	body, err := http.Post("http://localhost:2000/updateorder/", "application/json", bytes.NewBuffer(strJSON))
+	if err != nil {
+		t.Error(err)
+	}
+	b, _ := ioutil.ReadAll(body.Body)
+	if string(b) != "200" {
 		t.Error(string(b))
 	} else {
 		fmt.Println(string(b))
