1884506122adcce79dd3474542b5ce0649f45302
MrProstos <vladmihin28@gmail.com>
2022-05-22 17:06:03 +0300

    Изменил относительный путь на абсолютный

--- utils/utils.go
+++ utils/utils.go
@@ -15,7 +15,7 @@ func init() {
 	config.EncodeTime = zapcore.ISO8601TimeEncoder
 	fileEncoder := zapcore.NewJSONEncoder(config)
 	consoleEncoder := zapcore.NewConsoleEncoder(config)
-	logFile, err := os.OpenFile("log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	logFile, err := os.OpenFile("/home/Vlad/MrProstos/rest-api/log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
