e66038d76f7d0c9e3609bfe3203286ed0390ad1e
MrProstos <vladmihin28@gmail.com>
2022-05-26 04:34:58 +0300

    .

--- src/internal/db/db.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type ManageDb interface {
-	IsValid() error //Проверка на валидность данных
-	Show() error    //Получить данные клиента по номеру телефона
-	Add() error     //Добавляет клиента в базу данных
-	Update() error  //Обновление данных клиента
-	Del() error     //Удаление клиента
-}
